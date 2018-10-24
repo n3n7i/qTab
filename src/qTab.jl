4563b5baa2209a3caf6fd6bde1bb4042827346ed
n3n7i <25413904+n3n7i@users.noreply.github.com>
2018-10-24 11:59:36 +0800

    initial project upload

---
+++ src/qTab.jl
@@ -0,0 +1,33 @@
+
+module qTab
+
+function xcodegen(labels)
+
+  ob = "(";
+  cb = ")";
+  xstr = "" * ob;
+  for (i, j) in enumerate(labels)
+    xstr = xstr * j * " = datar[$(i)], ";
+    end;
+  xstr = xstr[1:end-2] * cb;
+  return xstr;
+  end;
+
+
+function qfunc(header)
+
+  dataexec = xcodegen(header);
+  myf = eval(Meta.parse(" function (datar) \n" * dataexec * "\n end;"));
+  return myf;
+  end; 
+
+
+function quickTable(data, xfun)
+
+  ret = [ xfun(data[i, :]) for i in 1:size(data,1)];  
+  return ret;
+  end;
+
+export quickTable, qfunc;
+
+end # module
